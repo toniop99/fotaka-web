98e47f9d94347e3c01ef225a98908e86de2e6ee2
Antonio Hernandez <granviamurcia2@gmail.com>
2021-12-31 20:36:12 +0100

    feat: added endpoints for management

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ApiUserController;
+use App\Http\Controllers\Api\BillController;
+use App\Http\Controllers\Api\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return 'jeee';
+Route::get('/check/token/{token?}', [ApiUserController::class, 'checkToken']);
+
+Route::middleware(['api'])->group(function () {
+    Route::post('/store/save/{token?}', [StoreController::class, 'save']);
+    Route::get('/store/get/all/{token?}', [StoreController::class, 'getAll']);
+    Route::delete('/store/delete/{token?}', [StoreController::class, 'delete']);
+
+    Route::post('/bill/save/{token?}', [BillController::class, 'save']);
+    Route::get('/bill/get/all/{token?}', [BillController::class, 'getAll']);
+    Route::get('/bill/get/store/{token?}', [BillController::class, 'getByStore']);
 });
 
 Route::get('/orlas/schools', 'App\Http\Controllers\Api\OrlasController@getSchoolsLinks');
